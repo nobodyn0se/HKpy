cbf4e3ed1f37d4ab79116dc0e59e0a2243eac2a6
nobodyn0se <beginnerd@github.com>
2019-05-09 23:16:44 +0530

    Solutions to Hackerrank Problems

---
+++ findAString.py
@@ -0,0 +1,11 @@
+def count_substring(string, sub_string):
+    count = 0
+    l = len(sub_string)
+    for a in range(0,len(string)):
+        if(string[a:a+l] == sub_string):
+            count += 1
+    return count
+
+
+    
+
